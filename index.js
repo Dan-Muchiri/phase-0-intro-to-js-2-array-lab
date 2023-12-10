0d8ca3c7e53ef40233f83850d199596d047e447f
Dan Muchiri <dan.muchiri@student.moringaschool.com>
2023-12-10 19:10:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name){
+    cats.push(name);
+    return (cats);
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+    return (cats);
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+    return (cats);
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+    return (cats);
+}
+function prependCat(name){
+    let copyOfCats = [name, ...cats];
+    return(copyOfCats);
+}
+function appendCat(name){
+    let copyOfCats = [...cats, name];
+    return(copyOfCats);
+}
+function removeLastCat(){
+    const removeLastCat = cats.slice(0, cats.length -1);
+    return(removeLastCat);
+}
+function removeFirstCat(){
+    const removeFirstCat = cats.slice(1);
+    return(removeFirstCat);
+}
